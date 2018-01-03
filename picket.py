e71cd40ef20634fe3f1323da16666164a4457a15
Sam Drew <sam-drew@outlook.com>
2018-01-03 12:18:58 +0000

    No more /0 Errors! Now uses a variation of standad form instead of p/s.

--- picket.py
+++ picket.py
@@ -3,9 +3,8 @@
 # tuple of (x, y).
 def in_fence(fence, point, verbose = False):
     # First, find the horozontal line equation that passes through the point.
-    # Using the y = mx + c fomat, express as a tuple of (m, c). As the line will
-    # always be horizontal, the equation will give constant output (y = n).
-    point_horizon_eqn = (0, point[1])
+    # Using the Ax + By = C fomat, express as a tuple of (A, B, C).
+    point_horizon_eqn = (0, 1, point[1])
 
     # Next, form equations with the list of points given by the Fence object.
     if len(fence.points) < 3:
@@ -24,20 +23,32 @@ def in_fence(fence, point, verbose = False):
             point2 = fence.points[point_index + 1]
         # Delta y over delta x == gradient.
         # Check if vertical or horizontal line first.
-        if point1[0] == point2[0]:
+        if point1[1] == point2[1]:
             # Hoizontal
+            a = 0
+            b = 1
             c = point1[1]
-            m = 0
-        elif point1[1] == point2[1]:
-            # Vertical; y = mx + c doesn't work.
-            # I've fucked this again. Need to change to Ax + By + C = 0. FFS!
-
-        c = ((point2[1] - point1[1]) / (point2[0] - point1[0]))
-        # Find m by substitution using point1.
-        m = ((point1[1] - c) / point1[0])
-        if verbose == True:
-            print("Formed equation y=" + m + "x+" + c, "from point:", point1, "and", point2)
-        line_eqns.append((m, c))
+            if verbose == True:
+                print("Formed equation", str(a) + "y+" + str(b) + "x=" + str(c), "from points:", point1, "and", point2)
+            line_eqns.append((a, b, c))
+        elif point1[0] == point2[0]:
+            # Vertical
+            a = 1
+            b = 0
+            c = point1[0]
+            if verbose == True:
+                print("Formed equation", str(a) + "y+" + str(b) + "x=" + str(c), "from points:", point1, "and", point2)
+            line_eqns.append((a, b, c))
+        else:
+            # Non vertical or hoizontal line.
+            a = 1
+            b = (-1 * ((point2[1] - point1[1]) / (point2[0] - point1[0])))
+            c = (point1[1] + (b * point1[0]))
+            if verbose == True:
+                print("Formed equation", str(a) + "y+" + str(b) + "x=" + str(c), "from points:", point1, "and", point2)
+            line_eqns.append((a, b, c))
+    if verbose == True:
+        print("All equations formed (in order):", line_eqns)
 
 # Class to define a fence.
 class Fence:
