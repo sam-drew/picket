cf0fc0deb0b00c34b46a69c6070f7960d186caf4
Sam Drew <sam-drew@outlook.com>
2018-01-03 02:01:46 +0000

    Now calcs line eqns for points in Fence.points. Fence.list_points works.

--- picket.py
+++ picket.py
@@ -1,7 +1,7 @@
 # in_fence checks if a given point lies inside a given fence polygon.
 # Parameters are given as an instance of the fence class and a given point as a
 # tuple of (x, y).
-def in_fence(fence, point):
+def in_fence(fence, point, verbose = False):
     # First, find the horozontal line equation that passes through the point.
     # Using the y = mx + c fomat, express as a tuple of (m, c). As the line will
     # always be horizontal, the equation will give constant output (y = n).
@@ -12,8 +12,23 @@ def in_fence(fence, point):
         raise Exception("The supplied fence has not enough (< 3) points!")
     # Form list of line equations. Last in list will circle back to first point.
     # E.g. fence.points = [(A), (B), (C)], line_eqns = [(AB), (BC), (CA)].
+    # Where each element in the line_eqns list is a tuple of (m, c).
     line_eqns = []
-
+    for point_index in range(len(fence.points)):
+        point1 = fence.points(point_index)
+        # If point1 is the last in the list, point2 is the first element of the
+        # list.
+        if point_index == (len(fence.points) - 1):
+            point2 = fence.points[0]
+        else:
+            point2 = fence.points(point_index + 1)
+        # Delta y over delta x == gradient.
+        c = ((point2[1] - point1[1]) / (point2[0] - point1[0]))
+        # Find m by substitution using point1.
+        m = ((point1[1] - c) / point1[0])
+        if vebose == True:
+            print("Formed equation y=" + m + "x+" + c, "from point:", point1, "and", point2)
+        line_eqns.append((m, c))
 
 # Class to define a fence.
 class Fence:
@@ -23,3 +38,5 @@ class Fence:
         self.points = []
     def add_point(self, point):
         self.points.append(point)
+    def list_points(self):
+        return(self.points)
--- test_picket.py
+++ test_picket.py
@@ -1,14 +1,25 @@
 import picket
 
-def test_ci():
-	assert 5 == 5
-
-def test_fence():
+def test_addpoints():
 	new_fence = picket.Fence()
 	try:
 		new_fence.add_point((1, 1))
 		new_fence.add_point((3, 1))
 		new_fence.add_point((3, 3))
 		new_fence.add_point((1, 3))
+		assert new_fence.points[0] == (1, 1)
+		assert new_fence.points[1] == (3, 1)
+		assert new_fence.points[2] == (3, 3)
+		assert new_fence.points[3] == (1, 3)
 	except:
 		assert 1 == 2
+
+def test_list_points():
+	my_fence = picket.Fence()
+
+	my_fence.add_point((1, 1))
+	my_fence.add_point((3, 1))
+	my_fence.add_point((3, 3))
+	my_fence.add_point((1, 3))
+
+	assert my_fence.list_points() == [(1, 1), (3, 1), (3, 3), (1, 3)]
