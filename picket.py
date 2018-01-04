917f72add222047ec9e9efa51275345be744b61d
Sam Drew <sam-drew@outlook.com>
2018-01-04 15:02:59 +0000

    Add licence boilerplate to file.

--- picket.py
+++ picket.py
@@ -1,3 +1,30 @@
+# Picket. Calculate if a point is inside a fence.
+# Copyright (C) 2017-2018  Sam Drew
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Class to define a fence.
+class Fence:
+    # Points are given as tuples
+    points = None
+    def __init__(self):
+        self.points = []
+    def add_point(self, point):
+        self.points.append(point)
+    def list_points(self):
+        return(self.points)
+
 # in_fence checks if a given point lies inside a given fence polygon.
 # Parameters are given as an instance of the fence class and a given point as a
 # tuple of (x, y).
@@ -89,14 +116,3 @@ def in_fence(fence, point, debug = False):
     else:
         print(intersection_points_left, intersection_points_right)
         return(False)
-
-# Class to define a fence.
-class Fence:
-    # Points are given as tuples
-    points = None
-    def __init__(self):
-        self.points = []
-    def add_point(self, point):
-        self.points.append(point)
-    def list_points(self):
-        return(self.points)
